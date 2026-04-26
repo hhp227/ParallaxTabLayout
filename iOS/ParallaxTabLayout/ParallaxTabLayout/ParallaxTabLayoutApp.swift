028bec2a2d57b2e1c24a618c15a10e0e3cb206e8
hhp227 <hong227@naver.com>
2026-04-26 15:40:38 +0900

    Initialize iOS

---
+++ iOS/ParallaxTabLayout/ParallaxTabLayout/ParallaxTabLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxTabLayoutApp.swift
+//  ParallaxTabLayout
+//
+//  Created by 홍희표 on 4/26/26.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxTabLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS/ParallaxTabLayout/ParallaxTabLayoutTests/ParallaxTabLayoutTests.swift
@@ -0,0 +1,16 @@
+//
+//  ParallaxTabLayoutTests.swift
+//  ParallaxTabLayoutTests
+//
+//  Created by 홍희표 on 4/26/26.
+//
+
+import Testing
+
+struct ParallaxTabLayoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
