366beba2ab50c1f979d080f2ddc615065db69beb
hhp227 <hong227@naver.com>
2026-04-26 23:23:51 +0900

    iOS 개선

--- iOS/ParallaxTabLayout/UI/Compat.swift
+++ iOS/ParallaxTabLayout/UI/Compat.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import UIKit
 
 extension View {
     @ViewBuilder
@@ -12,5 +13,21 @@ extension View {
 
     func foregroundColorCompat(_ color: Color) -> some View {
         foregroundColor(color)
+    }
+
+    func navigationBarTintColorCompat(_ color: UIColor) -> some View {
+        background(NavigationBarTintColorView(color: color))
+    }
+}
+
+private struct NavigationBarTintColorView: UIViewControllerRepresentable {
+    let color: UIColor
+
+    func makeUIViewController(context: Context) -> UIViewController {
+        UIViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        uiViewController.navigationController?.navigationBar.tintColor = color
     }
 }
